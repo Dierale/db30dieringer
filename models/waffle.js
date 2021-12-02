6098b74cb39e7a53b5020b6233312d2cc7a4e94a
Dierale <89657232+Dierale@users.noreply.github.com>
2021-12-01 22:33:19 -0600

    Added CRUD buttons to waffle view, added buttons to the views, added style to the stylesheet

--- models/waffle.js
+++ models/waffle.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const waffleSchema = new mongoose.Schema({
     size:{
         type: Number,
-        required: true,
-        min: 1,
-        max: 10
+        required: [true, 'Size from 1 to 10 required'],
+        min: [1, 'Size must be minimum 1'],
+        max: [10, 'Size cannot exceed 10']
     },
     color: String,
     cooking_state: String,
