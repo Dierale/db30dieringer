8380891197a3a9f6889fa99bfe913c10aca22efb
Dierale <89657232+Dierale@users.noreply.github.com>
2021-12-01 18:03:48 -0600

    Added a login and register page, updated the index to reflect those capabilities. Updated index routing for the proper pages. Installed passport related packages.

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// The Account model
+var Account =require('./models/account');
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username }, function( err, user) {
+      if (err) { return done(err); }
+      if (!user) { 
+        return done(null, false, {message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, {message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 // Establish connection to Atlas MongoDB
 const connectionString = process.env.MONGO_CON;
 mongoose = require('mongoose');
@@ -86,6 +107,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Create routers for web app
@@ -96,6 +124,13 @@ app.use('/addmods', modsRouter);
 app.use('/selector', selectRouter);
 app.use('/resource', resourceRouter);
 app.use('/waffles', wafflesRouter);
+
+// passport config
+// Use the existing connection
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
